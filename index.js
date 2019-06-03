41ac01727aca26c902fc92906fa173dcd72bf989
Anthea <anthea.mertn@gmail.com>
2019-06-03 11:24:50 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString){
+  const hour = timeString.split(':')[0];
+if (hour < 12 ) {
+  return 'Good Morning'}
+  else if (hour >=12 && hour <= 17) {
+    return 'Good Afternoon'}
+    else {
+    return 'Good Evening'
+  }
+}
+
+function displayMessage(string){
+  document.getElementById("greeting").innerText = string;
+}
