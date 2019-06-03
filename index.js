124223d46b4f78f4cb2002184e4cf0d518f4d565
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-03 10:21:58 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string) {
+  const time = parseInt(string, 10);
+  if(time < 12){
+    return "Good Morning"
+  } else if (time > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+};
+
+function displayMessage(string) {
+  let content = document.getElementById("greeting").innerText = string
+}
