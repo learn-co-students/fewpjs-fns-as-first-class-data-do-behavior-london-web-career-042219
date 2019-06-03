00b9490e00a1a65b1fb858c9a582fd3c337b8fa4
aidenjl193 <aidenjl193@gmail.com>
2019-06-03 11:13:00 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(message) {
+	document.getElementById("greeting").innerText = message;
+}
+
+function greet(time) {
+	let timeInteger = parseInt(time.replace(":", ""), 10);
+	if (timeInteger < 1200)
+		return "Good Morning";
+	if (timeInteger < 1700)
+		return "Good Afternoon"
+	return  "Good Evening";
+}
+
